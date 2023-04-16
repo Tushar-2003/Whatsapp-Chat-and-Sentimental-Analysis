89fe52290ef28852f895bf1b6f108cf513a97acb
YOUR_USERNAME <tushargupta3002@gmail.com>
2023-04-17 02:21:15 +0530

    fghjk

---
+++ visualization.py
@@ -0,0 +1,61 @@
+import os
+import numpy as np
+import pandas as pd
+from builddash import *
+from flask import render_template
+from senti import *
+xyz = 'word.csv'
+filename = os.path.join('csvs', xyz)
+print(filename)
+
+
+def build_dashboard(filename=filename):
+    df = pd.read_csv(filename)
+    contacts = np.unique(df["Contacts"]).shape[0]
+    pie(filename)
+    sentimental(filename)
+    most(filename)
+    word(filename)
+    week(filename)
+    monthsanal(filename)
+    msgs = number_of_msgs(filename)
+    member = number_of_unique_members(filename)
+    sdate = start_date(filename)
+    edate = end_date(filename)
+    avg = average_length_msg(filename)
+    maxl, name = max_length_msg(filename)
+    month = month_busy(filename)
+    day = weekday_busy(filename)
+
+    if np.unique(df["Contacts"]).shape[0] > 5:
+        least(filename)
+        return render_template(
+            "dash2.html",
+            filename=xyz,
+            msgs=msgs,
+            member=member,
+            sdate=sdate,
+            edate=edate,
+            day=day,
+            avg=avg,
+            maxl=maxl,
+            name=name,
+            month=month,
+        )
+    else:
+        return render_template(
+            "dash1.html",
+            filename=xyz,
+            msgs=msgs,
+            member=member,
+            sdate=sdate,
+            edate=edate,
+            day=day,
+            avg=avg,
+            maxl=maxl,
+            name=name,
+            month=month,
+        )
+        
+
+
